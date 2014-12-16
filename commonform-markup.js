a3041ebd3701e2e07e312a3458c6b9ef361d5eb4
Kyle Mitchell <kyle@kemitchell.com>
2014-12-15 16:34:50 -0800

    commentary

--- commonform-markup.js
+++ commonform-markup.js
@@ -1,5 +1,9 @@
 // commonform-markup.js
-// =============
+// ====================
+// Plain-text markup for Common Form forms
+//
+// License and other information available at:
+// https://github.com/commonform/commonform-markup.js
 (function() {
   'use strict';
 
@@ -29,97 +33,98 @@
     SUBJECT_OBJECT_MAP[REFERENCE] = 'summary';
     SUBJECT_OBJECT_MAP[INSERT] = 'field';
 
-    (function() {
-      // Regular expression string scanner
-      var Scanner = function(input) {
-        this.string = input;
-        this.tail = input;
-        this.offset = 0;
-      };
+    // ### Regular expression string scanner
+    var Scanner = function(input) {
+      this.string = input;
+      this.tail = input;
+      this.offset = 0;
+    };
 
-      Scanner.prototype.eos = function() {
-        return this.tail === '';
-      };
+    Scanner.prototype.eos = function() {
+      return this.tail === '';
+    };
 
-      Scanner.prototype.scan = function(re) {
-        var match = this.tail.match(re);
-        if (match && match.index === 0) {
-          var string = match[0];
-          this.tail = this.tail.substring(string.length);
-          this.offset += string.length;
-          return match;
-        }
-        return '';
-      };
-
-      Scanner.prototype.scanUntil = function(re) {
-        var match;
-        var index = this.tail.search(re);
-        if (index === -1) {
-          match = this.tail;
-          this.tail = '';
-        } else if (index === 0) {
-          match = '';
-        } else {
-          match = this.tail.substring(0, index);
-          this.tail = this.tail.substring(index);
-        }
-        this.offset += match.length;
+    Scanner.prototype.scan = function(re) {
+      var match = this.tail.match(re);
+      if (match && match.index === 0) {
+        var string = match[0];
+        this.tail = this.tail.substring(string.length);
+        this.offset += string.length;
         return match;
+      }
+      return '';
+    };
+
+    Scanner.prototype.scanUntil = function(re) {
+      var match;
+      var index = this.tail.search(re);
+      if (index === -1) {
+        match = this.tail;
+        this.tail = '';
+      } else if (index === 0) {
+        match = '';
+      } else {
+        match = this.tail.substring(0, index);
+        this.tail = this.tail.substring(index);
+      }
+      this.offset += match.length;
+      return match;
+    };
+
+    var VALID_PROPERTY_CHARS = '[ !#-;=?-@A-Z\\^-`a-z|-~]';
+
+    // API
+    // ---
+
+    exports.parseMarkup = (function() {
+      var TAG_RE = new RegExp(
+        '<(' + VALID_PROPERTY_CHARS + '+)>|' +
+        '""(' + VALID_PROPERTY_CHARS + '+)""|' +
+        '{(' + VALID_PROPERTY_CHARS + '+)}|' +
+        '\\[(' + VALID_PROPERTY_CHARS + '+)\\]'
+      );
+
+      var firstMatchGroup = function(match) {
+        for (var x = 1; x < match.length; x++) {
+          if (match[x]) {
+            return match[x];
+          }
+        }
       };
 
-      var VALID_PROPERTY_CHARS = '[ !#-;=?-@A-Z\\^-`a-z|-~]';
-
-      exports.parseMarkup = (function() {
-        var TAG_RE = new RegExp(
-          '<(' + VALID_PROPERTY_CHARS + '+)>|' +
-          '""(' + VALID_PROPERTY_CHARS + '+)""|' +
-          '{(' + VALID_PROPERTY_CHARS + '+)}|' +
-          '\\[(' + VALID_PROPERTY_CHARS + '+)\\]'
-        );
-
-        var firstMatchGroup = function(match) {
-          for (var x = 1; x < match.length; x++) {
-            if (match[x]) {
-              return match[x];
-            }
-          }
+      var objectForMatch = (function() {
+        var LEAD_CHAR_NOUN = {
+          '"': DEFINITION,
+          '<': USE,
+          '{': REFERENCE,
+          '[': INSERT
         };
 
-        var objectForMatch = (function() {
-          var LEAD_CHAR_NOUN = {
-            '"': DEFINITION,
-            '<': USE,
-            '{': REFERENCE,
-            '[': INSERT
-          };
-
-          return function(match) {
-            var value = firstMatchGroup(match);
-            var key = LEAD_CHAR_NOUN[match[0][0]];
-            var object = {};
-            object[key] = value;
-            return object;
-          };
-        })();
-
-        return function(input) {
-          var content = [];
-          var scanner = new Scanner(input);
-          while (!scanner.eos()) {
-            var value = scanner.scanUntil(TAG_RE);
-            if (value) {
-              content.push(value);
-            }
-            var match = scanner.scan(TAG_RE);
-            if (!match) {
-              break;
-            }
-            content.push(objectForMatch(match));
-          }
-          return { content: content };
+        return function(match) {
+          var value = firstMatchGroup(match);
+          var key = LEAD_CHAR_NOUN[match[0][0]];
+          var object = {};
+          object[key] = value;
+          return object;
         };
       })();
+
+      return function(input) {
+        var content = [];
+        var scanner = new Scanner(input);
+        while (!scanner.eos()) {
+          var value = scanner.scanUntil(TAG_RE);
+          if (value) {
+            content.push(value);
+          }
+          var match = scanner.scan(TAG_RE);
+          if (!match) {
+            break;
+          }
+          content.push(objectForMatch(match));
+        }
+        return { content: content };
+      };
     })();
 
     exports.toMarkup = (function() {
